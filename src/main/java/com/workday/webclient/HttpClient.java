75175dc8fedac8540cadf4604976da8f5b646aed
U-WORKDAYINTERNAL\andrew.fong <andrew.fong@AFONG-01.workdayinternal.com>
2015-08-13 13:01:52 -0700

    Simplify get json request can remove libs soon

--- src/main/java/com/workday/webclient/HttpClient.java
+++ src/main/java/com/workday/webclient/HttpClient.java
@@ -1,28 +1,38 @@
 package com.workday.webclient;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.*;
 
 public class HttpClient {
-	public static void main(String[] args) throws UnirestException, IOException {
-		HttpResponse<JsonNode> jsonResponse = Unirest.get("http://jsonplaceholder.typicode.com/posts").asJson();
-		System.out.println(jsonResponse.getStatus());
-		System.out.println(jsonResponse.getStatusText());
-
-		JSONObject j2 = jsonResponse.getBody().getObject();
-		JSONArray json = jsonResponse.getBody().getArray();
-//
-		System.out.println(json);
-		for(int i = 0; i < json.length(); i++) {
-			System.out.println(json.getJSONObject(i));
-		}
-
-		for(int i=0; i<json.length(); i++) {
-			System.out.println(json.getJSONObject(i).getString("title"));
-		}
+	int statusCode;
+	String statusText;
+	static JSONArray json;
+	
+	public static void main(String[] args) throws IOException, UnirestException {
+		HttpClient client = new HttpClient();
+		client.get("http://jsonplaceholder.typicode.com/posts");
+		System.out.println(client.statusCode);
+		System.out.println(client.statusText);
+		System.out.println(json.length());
+		System.out.println(json.getJSONObject(3));
+		
+//		for(int i = 0; i < json.length(); i++) {			
+//			System.out.println(json.getJSONObject(i));
+//		}
+//		for(int i=0; i<json.length(); i++) {
+//			System.out.println(json.getJSONObject(i).getString("title"));
+//		}		
 		Unirest.shutdown();
+	}
+	
+	public void get(String url) throws UnirestException {		
+		HttpResponse<JsonNode> resp = Unirest.get(url).asJson();
+		statusCode = resp.getStatus();
+		statusText = resp.getStatusText();
+		json = resp.getBody().getArray();		
 	}
 }
